c087ebb5afb4a4ea73477e57c7688e57b5d1bc06
Renan Florencio <renanflorenciodeoliveira@gmail.com>
2023-10-26 17:03:45 -0300

    Criação de classes Consumidor e Fabrica para Consumidor

---
+++ src/main/java/br/com/weon/testeconhecimentobackend/consumidor/Consumidor.java
@@ -0,0 +1,20 @@
+package br.com.weon.testeconhecimentobackend.consumidor;
+/**
+ * {@summary Consumidor}
+ * Classe responsável por consumir fila de objetos
+ */
+public class Consumidor implements IConsumidor {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void consumir() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/br/com/weon/testeconhecimentobackend/fabrica/FabricaConsumidor.java
@@ -0,0 +1,16 @@
+package br.com.weon.testeconhecimentobackend.fabrica;
+
+import java.util.List;
+
+import br.com.weon.testeconhecimentobackend.consumidor.IConsumidor;
+
+/**
+ * {@summary FabricaConsumidor}
+ * Classe de fábrica responsável por retornar instancias de Consumidor
+ */
+public class FabricaConsumidor {
+
+	public static List<IConsumidor> fabricarConsumidor(int quantidade){
+		
+	}
+}
