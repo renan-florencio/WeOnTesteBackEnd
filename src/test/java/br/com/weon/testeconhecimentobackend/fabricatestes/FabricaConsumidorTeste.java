f619ba33d2cd0590964d70a838bc9debca280d0b
Renan Florencio <renanflorenciodeoliveira@gmail.com>
2023-10-26 17:28:09 -0300

    Correção em método de teste de fabrica de consumidor

--- src/test/java/br/com/weon/testeconhecimentobackend/fabricatestes/FabricaConsumidorTeste.java
+++ src/test/java/br/com/weon/testeconhecimentobackend/fabricatestes/FabricaConsumidorTeste.java
@@ -7,10 +7,13 @@ import java.security.InvalidParameterException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import br.com.weon.testeconhecimentobackend.consumidor.IConsumidor;
+import br.com.weon.testeconhecimentobackend.fabrica.FabricaConsumidor;
+
 class FabricaConsumidorTeste {
 
 	@BeforeAll
-	void retornaInstanciaDeIConsumidor() {
+	static void retornaInstanciaDeIConsumidor() {
 		assertTrue(FabricaConsumidor.fabricarConsumidor(1).get(0) instanceof IConsumidor,
 				"A fabrica não retornou uma instancia de IConsumidor!");
 	}
@@ -23,13 +26,23 @@ class FabricaConsumidorTeste {
 	
 	@Test
 	void retornaTresInstanciasDeIConsumidor() {
-		assertTrue(FabricaConsumidor.fabricarConsumidor(1).size() == 3,
+		assertTrue(FabricaConsumidor.fabricarConsumidor(3).size() == 3,
 				"A fabrica não retornou três instancias de IConsumidor!");
 	}
 	
 	@Test
 	void lancaInvalidParameterException() {
-		assertThrows(InvalidParameterException.class, () ->{}, 
+		assertThrows(InvalidParameterException.class, () -> FabricaConsumidor.fabricarConsumidor(0), 
 				"A fabrica não retornou lançou uma InvalidParameterException ao tentar criar 0 instancias de IConsumidor!");
+	}
+	
+	@Test
+	void validaMensagemDeExcecaoLancada() {
+		Exception exception = assertThrows(InvalidParameterException.class, () -> FabricaConsumidor.fabricarConsumidor(-1));
+		
+		String mensagemAtual = exception.getMessage();
+		String mensagemEsperada = "Não é possível criar -1 instancias de IConsumidor";
+		
+		assertEquals(mensagemAtual,mensagemEsperada);
 	}
 }
